e84e14dc09a84aec36e69f3f8444cd1798240e30
aryeh <aryeh.armon@gmail.com>
2015-06-23 13:02:27 +0300

    added angularJS app tag to <html> tag

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> ng-app="podium">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
