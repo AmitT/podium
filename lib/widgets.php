489cda5e652e75b71a28873ef1bd93dd8fbfde60
aryeh <aryeh.armon@gmail.com>
2015-06-23 16:55:03 +0300

    Removed the off canvas widget.

--- lib/widgets.php
+++ lib/widgets.php
@@ -14,17 +14,8 @@ function podium_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-		));
-	register_sidebar(array(
-		'id'			=> 'offcanvas',
-		'name'			=> esc_html__('Offcanvas', 'podium'),
-		'description' 	=> esc_html__('The offcanvas sidebar.', 'podium'),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' 	=> '</div>',
-		'before_title' 	=> '<h4 class="widget-title">',
-		'after_title'	=> '</h4>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 		));
 }
 add_action( 'widgets_init', 'podium_widgets_init' );
