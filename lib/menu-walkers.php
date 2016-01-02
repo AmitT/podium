b728a2d7fe4d3a8f3149cb9836e6d9073cb0aeac
AmitT <amitprobable@gmail.com>
2016-01-02 19:44:16 +0200

    onCanvas menu updated to F6 html syntax

--- lib/menu-walkers.php
+++ lib/menu-walkers.php
@@ -5,16 +5,16 @@ final class Top_Bar_Walker extends Walker_Nav_Menu {
 	function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
 		$element->has_children = ! empty( $children_elements[ $element->ID ] );
 		$element->classes[] = ( $element->current || $element->current_item_ancestor ) ? 'active' : '';
-		$element->classes[] = ( $element->has_children && 1 !== $max_depth ) ? 'has-dropdown' : '';
+		$element->classes[] = ( $element->has_children && 1 !== $max_depth ) ? 'has-submenu' : '';
 		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 	}
 	function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
 		$item_html = '';
 		parent::start_el( $item_html, $object, $depth, $args );
-		$output .= ( 0 == $depth ) ? '<li class="divider"></li>' : '';
+		//$output .= ( 0 == $depth ) ? '<li class="divider"></li>' : '';
 		$classes = empty( $object->classes ) ? array() : (array) $object->classes;
 		if ( in_array( 'label', $classes ) ) {
-			$output .= '<li class="divider"></li>';
+			//$output .= '<li class="divider"></li>';
 			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '<label>$1</label>', $item_html );
 		}
 	if ( in_array( 'divider', $classes ) ) {
@@ -23,7 +23,7 @@ final class Top_Bar_Walker extends Walker_Nav_Menu {
 	$output .= $item_html;
 	}
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$output .= "\n<ul class=\"sub-menu dropdown\">\n";
+		$output .= "\n<ul class=\"submenu menu vertical\">\n";
 	}
 }
 
