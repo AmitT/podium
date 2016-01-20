2389b4270e3dfdc23709f00bee676a514639c0fd
AmitT <amitprobable@gmail.com>
2016-01-20 19:37:42 +0200

    כשהןבםמד ךםבשאןםמ כןסקג

--- header.php
+++ header.php
@@ -21,24 +21,24 @@ $settings = new settings();
 	<!-- Please create favicon files with http://iconogen.com/
 	and put them in assets/images/favicon directory -->
 
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" type="image/x-icon" />
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon-16x16.png" sizes="16x16">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/android-chrome-192x192.png" sizes="192x192">
-	<meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/smalltile.png" />
-	<meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/mediumtile.png" />
-	<meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/widetile.png" />
-	<meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/largetile.png" />
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/android-chrome-192x192.png" sizes="192x192">
+	<meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/smalltile.png" />
+	<meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/mediumtile.png" />
+	<meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/widetile.png" />
+	<meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/largetile.png" />
 </head>
 
 <body <?php body_class(); ?>>
@@ -47,7 +47,7 @@ $settings = new settings();
 			<a class="skip-link screen-reader-text hide" href="#content"><?php esc_html_e( 'Skip to content', 'podium' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				
+
 				<div class="top-bar">
 					<div class="row">
 						<div class="small-12 columns">
