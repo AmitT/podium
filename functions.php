9cc36290a0848556f1b01a18f32e4c372ab54623
AmitT <amitprobable@gmail.com>
2016-03-16 22:49:02 +0200

    disable updates on server -- updates only via it --

--- functions.php
+++ functions.php
@@ -1,11 +1,23 @@
 <?php
 /**
- * podium functions and definitions
- *
- * @package podium
- */
+* podium functions and definitions
+*
+* @package podium
+*/
 
-define('WP_ENV', 'development'); // remove when you make the site live.
+// Set host name
+$host = $_SERVER['SERVER_NAME'];
+
+if($host === 'localhost'){
+	//set development ENV
+	define('WP_ENV', 'development'); // remove when you make the site live.
+} else {
+
+	// Disable updates on server UPDATE ONLY WITH GIT
+	require get_template_directory() . '/lib/disable-updates.php';
+	// Set production ENV
+	define('WP_ENV', 'production');
+}
 
 if (!defined('WP_ENV')) {
 	// Fallback if WP_ENV isn't defined in your WordPress config
@@ -15,32 +27,32 @@ if (!defined('WP_ENV')) {
 
 // TODO move to seperate files
 function podium_scripts() {
-  if(is_rtl()){
-    // Load RTL Styles
-    if (WP_ENV !== 'development'){
-      wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.min.css' );
-    } else {
-      wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.css' );
-    }
-  } else {
-    // Load LTR Styles
-    if (WP_ENV !== 'development'){
-      wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.min.css' );
-    } else {
-      wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.css' );
-    }
-  }
-  if (WP_ENV !== 'development'){
-    wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.min.js', array(), '20120206', true );
-  } else {
-    wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.js', array(), '20120206', true );
-  }
-  //wp_enqueue_script( 'podium-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-  //wp_enqueue_script( 'podium-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	if(is_rtl()){
+		// Load RTL Styles
+		if (WP_ENV !== 'development'){
+			wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.min.css' );
+		} else {
+			wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.css' );
+		}
+	} else {
+		// Load LTR Styles
+		if (WP_ENV !== 'development'){
+			wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.min.css' );
+		} else {
+			wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.css' );
+		}
+	}
+	if (WP_ENV !== 'development'){
+		wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.min.js', array(), '20120206', true );
+	} else {
+		wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.js', array(), '20120206', true );
+	}
+	//wp_enqueue_script( 'podium-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	//wp_enqueue_script( 'podium-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-    wp_enqueue_script( 'comment-reply' );
-  }
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'podium_scripts' );
 
@@ -61,7 +73,8 @@ $reqireFiles = [
 	'/lib/pagination.php', // More flexible pagination function
 
 	'/lib/config.php',          // get the settings for the wordpress theme.
-	                            // this file should be edited to meet the needs of the theme.
+	// this file should be edited to meet the needs of the theme.
+
 ];
 
 // require all the files in the $reqireFiles array
