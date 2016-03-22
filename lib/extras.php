a9993d5ab3e4c5e531ec28a0ae0cd53ac66e85ee
AmitT <amitprobable@gmail.com>
2016-03-22 18:26:51 +0200

    Image in CF7 email

--- lib/extras.php
+++ lib/extras.php
@@ -80,3 +80,34 @@ function get_thumb_url($post, $size = 'full'){
 	}
 	return false;
 }
+
+// Image in CF7 email
+function winsite_wpcf7_special_mail_tag( $output, $name, $html ) {
+    $submission = WPCF7_Submission::get_instance();
+
+    if ( ! $submission ) {
+        return $output;
+    }
+
+    if ( '_post_thumbnail' == $name ) {
+        $unit_tag = $submission->get_meta( 'unit_tag' );
+
+        if ( $unit_tag
+             && preg_match( '/^wpcf7-f(\d+)-p(\d+)-o(\d+)$/', $unit_tag, $matches ) ) {
+            $post_id = absint( $matches[2] );
+
+            if ( $post = get_post( $post_id ) ) {
+                $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+                $image = wp_get_attachment_image_src( $post_thumbnail_id, 'small' );
+                if ( isset( $image[0] ) ) {
+                    return '<img src="' . $image[0] . '" width="120" />';
+                }
+            }
+        }
+    }
+
+    return $output;
+}
+add_filter( 'wpcf7_special_mail_tags', 'winsite_wpcf7_special_mail_tag', 10, 3 );
+// usu in mail:
+//[_post_thumbnail]
