7c5193345d0c9094f52d1e8c963ab0bdbe618e82
AmitT <amitprobable@gmail.com>
2016-04-12 21:24:59 +0300

    trim words in admin dashbords winsite widget

--- lib/admin.php
+++ lib/admin.php
@@ -47,7 +47,8 @@ function podium_rss_dashboard_widget() {
 			</a>
 		</h4>
 		<p style="margin-top: 0.5em;">
-			<?php echo strip_tags(substr($item->get_description(), 0, 200)); ?>
+			<?php echo strip_tags(wp_trim_words( $item->get_description(), 40, '...' )); 
+			?>
 			<a style="display:block;" href="<?php echo $item->get_permalink(); ?>" title="<?php echo __('Read More', 'podium'); ?>" target="_blank">
 			<?php echo __('Read More', 'podium'); ?> >
 		</a>
