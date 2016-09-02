cf99586df29771e91f41b9824611e5e21f3c232f
AmitT <amitprobable@gmail.com>
2016-09-02 10:12:39 +0300

    removed scsslint from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -78,9 +78,9 @@ gulp.task( 'rtl-styles', function() {
 
 	gulp.src( scss_files )
 	.pipe( sourcemaps.init() )
-	.pipe( scsslint( {
-		'config': 'sass-lint.yml',
-	} ) )
+	// .pipe( scsslint( {
+	// 	'config': 'sass-lint.yml',
+	// } ) )
 	.pipe( sass() ).on( "error", notify.onError( function ( error ) {
 		let filename = error.fileName.replace(/^.*[\\\/]/, '')
 		return "SASS error:\n" + filename + "\nLine " +  error.lineNumber;
