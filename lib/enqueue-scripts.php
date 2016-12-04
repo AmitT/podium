089085757f6254acaf61c028e4caabef6d6c034b
Amit <amit@win-site.co.il>
2016-12-04 10:32:44 +0200

    seperate enque files from main functions file, better documentation

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -7,20 +7,52 @@
 * @package podium
 */
 
+// Set host name
+$host = $_SERVER['SERVER_NAME'];
+
+if ( $host === 'localhost' || $host === 'win-sites.co.il' || $host === 'win-site.info' ) {
+	//set development ENV
+	define( 'WP_ENV', 'development' ); // remove when you make the site live.
+} else {
+	// Disable updates on server UPDATE ONLY WITH GIT
+	// require get_template_directory() . '/lib/disable-updates.php';
+	// Set production ENV
+	define( 'WP_ENV', 'production' );
+}
+
+if ( ! defined( 'WP_ENV' ) ) {
+	// Fallback if WP_ENV isn't defined in your WordPress config
+	// Used to check for 'development' or 'production'
+	define( 'WP_ENV', 'production' );
+}
+
+// TODO move to seperate files
 function podium_scripts() {
-  if(is_rtl()){
-    wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.min.css' );
-  } else {
-    wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.min.css' );
-  }
+	if ( is_rtl() ) {
+		// Load RTL Styles
+		if ( WP_ENV !== 'development' ) {
+			wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.min.css' );
+		} else {
+			wp_enqueue_style( 'podium-rtl-style', get_stylesheet_directory_uri() . '/dist/styles/rtl.css' );
+		}
+	} else {
+		// Load LTR Styles
+		if ( WP_ENV !== 'development' ) {
+			wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.min.css' );
+		} else {
+			wp_enqueue_style( 'podium-style', get_stylesheet_directory_uri() . '/dist/styles/main.css' );
+		}
+	}
+	if ( WP_ENV !== 'development' ) {
+		wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.min.js', array(), '20120206', true );
+	} else {
+		wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.js', array(), '20120206', true );
+	}
+	//wp_enqueue_script( 'podium-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	//wp_enqueue_script( 'podium-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-  wp_enqueue_script( 'podium-navigation', get_stylesheet_directory_uri() . '/dist/scripts/main.min.js', array(), '20120206', true );
-
-  //wp_enqueue_script( 'podium-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-  //wp_enqueue_script( 'podium-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-    wp_enqueue_script( 'comment-reply' );
-  }
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'podium_scripts' );
