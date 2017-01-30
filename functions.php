fbbc8f872e75cfab887697f2f1cbc99a45657bc3
Amit Tal <amitprobable@gmail.com>
2017-01-30 20:59:30 +0200

    lib replace with inc to compatible _s and wordpress conventions

--- functions.php
+++ functions.php
@@ -7,22 +7,22 @@
 
 // require array list
 $reqire_files = [
-	'/lib/enqueue-scripts.php', // Enqueue scripts and styles.
-	'/lib/cleanup.php', // cleanup & setup theme.
-	'/lib/widgets.php',         // Implement Custom widgets.
-	'/lib/template-tags.php',   // Custom template tags for this theme.
-	'/lib/extras.php',          // Custom functions that act independently of the theme templates.
-	'/lib/customizer.php',      // Customizer additions.
-	'/lib/menu-walkers.php',    // TODO -> make comment describing the functionality of the page
-	'/lib/menu.php',            // TODO -> make comment describing the functionality of the page
-	'/lib/admin.php',           // Code for better handaling the admin area
-	'/lib/custom-fields.php',   // Inintialize custom fields (if you prefer to do this without ACF)
-	'/lib/custom-post-type.php', // Inintialize unique CPT's and taxonomies for this project
-	'/lib/pagination.php', // More flexible pagination function
+	'/inc/enqueue-scripts.php', // Enqueue scripts and styles.
+	'/inc/cleanup.php', // cleanup & setup theme.
+	'/inc/widgets.php',         // Implement Custom widgets.
+	'/inc/template-tags.php',   // Custom template tags for this theme.
+	'/inc/extras.php',          // Custom functions that act independently of the theme templates.
+	'/inc/customizer.php',      // Customizer additions.
+	'/inc/menu-walkers.php',    // TODO -> make comment describing the functionality of the page
+	'/inc/menu.php',            // TODO -> make comment describing the functionality of the page
+	'/inc/admin.php',           // Code for better handaling the admin area
+	'/inc/custom-fields.php',   // Inintialize custom fields (if you prefer to do this without ACF)
+	'/inc/custom-post-type.php', // Inintialize unique CPT's and taxonomies for this project
+	'/inc/pagination.php', // More flexible pagination function
 
-	'/lib/clean-filenames.php', // Custom functions to clean filenames from Unicode to ASCII
-	'/lib/config.php',          // get the settings for the wordpress theme.
-	'/lib/media.php',          // Media functions.
+	'/inc/clean-filenames.php', // Custom functions to clean filenames from Unicode to ASCII
+	'/inc/config.php',          // get the settings for the wordpress theme.
+	'/inc/media.php',          // Media functions.
 
 	// plugins:
 	'/plugins/tgm/podium-tpm.php', // Plugin installation and activation for Podium based themes.
@@ -38,7 +38,7 @@ foreach ( $reqire_files as $file ) {
 
 // Include array list
 $include_files = [
-	//'lib/xxxxxxxxxxxxxxxxxxxxxx.php',
+	//'inc/xxxxxxxxxxxxxxxxxxxxxx.php',
 ];
 
 // Include all the files in the $include_files array
