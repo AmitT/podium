b36c787d07a19b4d3629e99bc0a89166d33ee2b7
Amit Tal <amitprobable@gmail.com>
2017-02-08 14:36:37 +0200

    functions updated

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -8,9 +8,7 @@
 * @package podium
 */
 
-if ( !function_exists( 'the_posts_navigation' ) ) {
-
-	if ( !function_exists( 'podium_posted_on' ) ) {
+if ( !function_exists( 'podium_posted_on' ) ) {
 
 			/**
 			* Prints HTML with meta information for the current post-date/time and author.
@@ -21,26 +19,21 @@ if ( !function_exists( 'the_posts_navigation' ) ) {
 				if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 					$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 				}
-
 				$time_string = sprintf( $time_string,
 					esc_attr( get_the_date( 'c' ) ),
 					esc_html( get_the_date() ),
 					esc_attr( get_the_modified_date( 'c' ) ),
 					esc_html( get_the_modified_date() )
 					);
-
 				$posted_on = sprintf(
-					esc_html_x( 'Posted on %s', 'post date', 'podium' ),
+					esc_html_x( 'Posted on %s', 'post date', '_s' ),
 					'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 					);
-
 				$byline = sprintf(
-					esc_html_x( 'by %s', 'post author', 'podium' ),
+					esc_html_x( 'by %s', 'post author', '_s' ),
 					'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 					);
-
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
 }
 }
 
@@ -51,30 +44,35 @@ if ( !function_exists( 'podium_entry_footer' ) ) {
 	*/
 	function podium_entry_footer() {
 
-		// Hide category and tag text for pages.
-		if ( 'post' == get_post_type() ) {
-
+	// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'podium' ) );
-			if ( $categories_list && podium_categorized_blog() ) {
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'podium' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-			}
-
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html__( ', ', 'podium' ) );
-			if ( $tags_list ) {
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'podium' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-			}
+			$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
+			if ( $categories_list && _s_categorized_blog() ) {
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
-
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
-			comments_popup_link( esc_html__( 'Leave a comment', 'podium' ), esc_html__( '1 Comment', 'podium' ), esc_html__( '% Comments', 'podium' ) );
-			echo '</span>';
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', '_s' ) );
+		if ( $tags_list ) {
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
-
-		edit_post_link( esc_html__( 'Edit', 'podium' ), '<span class="edit-link">', '</span>' );
 	}
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		/* translators: %s: post title */
+		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+		echo '</span>';
+	}
+	edit_post_link(
+		sprintf(
+			/* translators: %s: Name of current post */
+			esc_html__( 'Edit %s', '_s' ),
+			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			),
+		'<span class="edit-link">',
+		'</span>'
+		);
+}
 }
 
 /**
