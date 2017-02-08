7d16da9f69bf0b1edc7d0319d36c6db1d9cf8a4c
Amit Tal <amitprobable@gmail.com>
2017-02-08 14:38:52 +0200

    functions updated

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -26,11 +26,11 @@ if ( !function_exists( 'podium_posted_on' ) ) {
 					esc_html( get_the_modified_date() )
 					);
 				$posted_on = sprintf(
-					esc_html_x( 'Posted on %s', 'post date', '_s' ),
+					esc_html_x( 'Posted on %s', 'post date', 'podium' ),
 					'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 					);
 				$byline = sprintf(
-					esc_html_x( 'by %s', 'post author', '_s' ),
+					esc_html_x( 'by %s', 'post author', 'podium' ),
 					'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 					);
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
@@ -47,26 +47,26 @@ if ( !function_exists( 'podium_entry_footer' ) ) {
 	// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
-			if ( $categories_list && _s_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			$categories_list = get_the_category_list( esc_html__( ', ', 'podium' ) );
+			if ( $categories_list && podium_categorized_blog() ) {
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'podium' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
 		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', '_s' ) );
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'podium' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'podium' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
 		/* translators: %s: post title */
-		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'podium' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
 		echo '</span>';
 	}
 	edit_post_link(
 		sprintf(
 			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', '_s' ),
+			esc_html__( 'Edit %s', 'podium' ),
 			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			),
 		'<span class="edit-link">',
