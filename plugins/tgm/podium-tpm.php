f80d304225b97c9004e13453853234b8b7d3e8b2
Amit <amit@win-site.co.il>
2017-03-30 18:26:00 +0300

    reorder TGM file

--- plugins/tgm/podium-tpm.php
+++ plugins/tgm/podium-tpm.php
@@ -24,6 +24,7 @@
 require_once 'class-tgm-plugin-activation.php';
 
 add_action('tgmpa_register', 'podium_register_required_plugins');
+
 /**
  * Register the required plugins for this theme.
  *
@@ -38,71 +39,20 @@ add_action('tgmpa_register', 'podium_register_required_plugins');
  * This function is hooked into tgmpa_init, which is fired within the
  * TGM_Plugin_Activation class constructor.
  */
+
 function podium_register_required_plugins()
 {
+
     /*
      * Array of plugin arrays. Required keys are name and slug.
      * If the source is NOT from the .org repo, then source is also required.
      */
     $plugins = [
-
-        // This is an example of how to include a plugin bundled with a theme.
-        // array(
-        //     'name'               => 'TGM Example Plugin', // The plugin name.
-        //     'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-        //     'source'             => get_template_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
-        //     'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-        //     'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-        //     'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-        //     'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-        //     'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-        //     'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-        // ),
-
-        // This is an example of how to include a plugin from an arbitrary external source in your theme.
-        // array(
-        //     'name'         => 'TGM New Media Plugin', // The plugin name.
-        //     'slug'         => 'tgm-new-media-plugin', // The plugin slug (typically the folder name).
-        //     'source'       => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', // The plugin source.
-        //     'required'     => true, // If false, the plugin is only 'recommended' instead of required.
-        //     'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
-        // ),
-
-        // This is an example of how to include a plugin from a GitHub repository in your theme.
-        // This presumes that the plugin code is based in the root of the GitHub repository
-        // and not in a subdirectory ('/src') of the repository.
-        // array(
-        //     'name'      => 'Adminbar Link Comments to Pending',
-        //     'slug'      => 'adminbar-link-comments-to-pending',
-        //     'source'    => 'https://github.com/jrfnl/WP-adminbar-comments-to-pending/archive/master.zip',
-        // ),
-
-        // This is an example of how to include a plugin from the WordPress Plugin Repository.
-        // array(
-        //     'name'      => 'BuddyPress',
-        //     'slug'      => 'buddypress',
-        //     'required'  => false,
-        // ),
         [
             'name'     => 'Aryo Activity Log',
             'slug'     => 'aryo-activity-log',
             'required' => false
         ],
-        // array(
-        //     'name'      => 'Contact Form 7',
-        //     'slug' => 'contact-form-7',
-        //     'required'  => false,
-        // ),
-        // array(
-        //     'name'      => 'Contact Form 7 Honeypot',
-        //     'slug' => 'contact-form-7-honeypot',
-        //     'required'  => false,
-        // ),
-        // array(
-        //     'name'      => 'Contact Form DB',
-        //     'slug' => 'contact-form-7-to-database-extension',
-        //     'required'  => false,
-        // ),
         [
             'name'     => 'Redirection',
             'slug'     => 'redirection',
@@ -145,27 +95,15 @@ function podium_register_required_plugins()
             'slug'             => 'winsite-image-optimizer',
             'required'         => true,
             'force_activation' => true
-        ]
+        ];
 
-        // array(
-        //     'name'      => 'Polylang',
-        //     'slug' => 'polylang',
-        //     'source' => '',
-        //     'required'  => true,
-        //     'force_activation' => true,
-        // ),
-
-        // This is an example of the use of 'is_callable' functionality. A user could - for instance -
-        // have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.
-        // 'wordpress-seo-premium'.
-        // By setting 'is_callable' to either a function from that plugin or a class method
-        // `array( 'class', 'method' )` similar to how you hook in to actions and filters, TGMPA can still
-        // recognize the plugin as being installed.
-        // array(
-        //     'name'        => 'WordPress SEO by Yoast',
-        //     'slug'        => 'wordpress-seo',
-        //     'is_callable' => 'wpseo_init',
-        // ),
+        // [
+        //     'name'             => 'Polylang',
+        //     'slug'             => 'polylang',
+        //     'source'           => '',
+        //     'required'         => true,
+        //     'force_activation' => true
+        // ]
 
     ];
 
@@ -189,84 +127,6 @@ function podium_register_required_plugins()
         'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
         'is_automatic' => false,                   // Automatically activate plugins after installation or not.
         'message'      => ''                      // Message to output right before the plugins table.
-
-        /*
-    'strings'      => array(
-    'page_title'                      => __( 'Install Required Plugins', 'podium' ),
-    'menu_title'                      => __( 'Install Plugins', 'podium' ),
-    'installing'                      => __( 'Installing Plugin: %s', 'podium' ), // %s = plugin name.
-    'oops'                            => __( 'Something went wrong with the plugin API.', 'podium' ),
-    'notice_can_install_required'     => _n_noop(
-    'This theme requires the following plugin: %1$s.',
-    'This theme requires the following plugins: %1$s.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_can_install_recommended'  => _n_noop(
-    'This theme recommends the following plugin: %1$s.',
-    'This theme recommends the following plugins: %1$s.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_cannot_install'           => _n_noop(
-    'Sorry, but you do not have the correct permissions to install the %1$s plugin.',
-    'Sorry, but you do not have the correct permissions to install the %1$s plugins.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_ask_to_update'            => _n_noop(
-    'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-    'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_ask_to_update_maybe'      => _n_noop(
-    'There is an update available for: %1$s.',
-    'There are updates available for the following plugins: %1$s.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_cannot_update'            => _n_noop(
-    'Sorry, but you do not have the correct permissions to update the %1$s plugin.',
-    'Sorry, but you do not have the correct permissions to update the %1$s plugins.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_can_activate_required'    => _n_noop(
-    'The following required plugin is currently inactive: %1$s.',
-    'The following required plugins are currently inactive: %1$s.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_can_activate_recommended' => _n_noop(
-    'The following recommended plugin is currently inactive: %1$s.',
-    'The following recommended plugins are currently inactive: %1$s.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'notice_cannot_activate'          => _n_noop(
-    'Sorry, but you do not have the correct permissions to activate the %1$s plugin.',
-    'Sorry, but you do not have the correct permissions to activate the %1$s plugins.',
-    'podium'
-    ), // %1$s = plugin name(s).
-    'install_link'                    => _n_noop(
-    'Begin installing plugin',
-    'Begin installing plugins',
-    'podium'
-    ),
-    'update_link'                       => _n_noop(
-    'Begin updating plugin',
-    'Begin updating plugins',
-    'podium'
-    ),
-    'activate_link'                   => _n_noop(
-    'Begin activating plugin',
-    'Begin activating plugins',
-    'podium'
-    ),
-    'return'                          => __( 'Return to Required Plugins Installer', 'podium' ),
-    'plugin_activated'                => __( 'Plugin activated successfully.', 'podium' ),
-    'activated_successfully'          => __( 'The following plugin was activated successfully:', 'podium' ),
-    'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'podium' ),  // %1$s = plugin name(s).
-    'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'podium' ),  // %1$s = plugin name(s).
-    'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'podium' ), // %s = dashboard link.
-    'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'podium' ),
-
-    'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
-    ),
-     */
     ];
 
     tgmpa($plugins, $config);
