8e6791f9f158a08f0b7876cd7798d64e4c3ad3e3
Amit Tal <amitprobable@gmail.com>
2017-03-31 10:20:41 +0300

    better sementics on archives, edit link commented out

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -79,163 +79,222 @@ if (!function_exists('podium_entry_footer')) {
             echo '<span class="comments-link">';
             /* translators: %s: post title */
             comments_popup_link(sprintf(wp_kses(__('Leave a Comment<span class="screen-reader-text"> on %s</span>', 'podium'), ['span' => ['class' => []]]), get_the_title()));
-            echo '</span>';
+            echo '</span> ';
 
         }
 
-        edit_post_link(
-            sprintf(
-                /* translators: %s: Name of current post */
-                esc_html__('Edit %s', 'podium'),
-                the_title('<span class="screen-reader-text">"', '"</span>', false)
-            ),
-            '<span class="edit-link">',
-            '</span>'
-        );
+// edit_post_link(
+
+//     sprintf(
+
+//         /* translators: %s: Name of current post */
+
+//         esc_html__('Edit %s', 'podium'),
+
+//         the_title('<span class="screen-reader-text">"', '"</span>', false)
+
+//     ),
+
+//     '<span class="edit-link">',
+
+//     '</span>'
+        // );
     }
 
 }
+
+if (!function_exists('podium_categorized_blog')) {
 
 /**
  * Returns true if a blog has more than 1 category.
  *
  * @return bool
  */
-function podium_categorized_blog()
-{
+    function podium_categorized_blog()
+    {
 
-    if (false === ($all_the_cool_cats = get_transient('podium_categories'))) {
+        if (false === ($all_the_cool_cats = get_transient('podium_categories'))) {
 
-        // Create an array of all the categories that are attached to posts.
-        $all_the_cool_cats = get_categories([
-            'fields'     => 'ids',
-            'hide_empty' => 1,
+            // Create an array of all the categories that are attached to posts.
+            $all_the_cool_cats = get_categories([
+                'fields'     => 'ids',
+                'hide_empty' => 1,
 
-            // We only need to know if there is more than one category.
-            'number'     => 2
-        ]);
+                // We only need to know if there is more than one category.
+                'number'     => 2
+            ]);
 
-        // Count the number of categories that are attached to the posts.
-        $all_the_cool_cats = count($all_the_cool_cats);
+            // Count the number of categories that are attached to the posts.
+            $all_the_cool_cats = count($all_the_cool_cats);
 
-        set_transient('podium_categories', $all_the_cool_cats);
-    }
+            set_transient('podium_categories', $all_the_cool_cats);
+        }
 
-    if ($all_the_cool_cats > 1) {
+        if ($all_the_cool_cats > 1) {
 
-        // This blog has more than 1 category so podium_categorized_blog should return true.
-        return true;
+            // This blog has more than 1 category so podium_categorized_blog should return true.
+            return true;
 
-    } else {
+        } else {
 
-        // This blog has only 1 category so podium_categorized_blog should return false.
-        return false;
+            // This blog has only 1 category so podium_categorized_blog should return false.
+            return false;
+
+        }
 
     }
 
 }
 
+if (!function_exists('podium_category_transient_flusher')) {
+
 /**
  * Flush out the transients used in podium_categorized_blog.
  */
-function podium_category_transient_flusher()
-{
+    function podium_category_transient_flusher()
+    {
 
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
 
-        return;
+            return;
 
+        }
+
+        // Like, beat it. Dig?
+        delete_transient('podium_categories');
     }
 
-    // Like, beat it. Dig?
-    delete_transient('podium_categories');
 }
 
 add_action('edit_category', 'podium_category_transient_flusher');
 add_action('save_post', 'podium_category_transient_flusher');
 
+if (!function_exists('post_end_class')) {
+
 /**
  * @return mixed
  */
-function post_end_class()
-{
+    function post_end_class()
+    {
 
 // returns .end to the last post
 
 // add in category content page if you have more then one post in row
 
-    if ($wp_query->current_post + 1 == $wp_query->post_count) {
+        if ($wp_query->current_post + 1 == $wp_query->post_count) {
 
-        $end = ' end';
+            $end = ' end';
 
-    } else {
+        } else {
 
-        $end = '';
+            $end = '';
 
+        }
+
+        return $end;
     }
 
-    return $end;
 }
+
+if (!function_exists('podium_archive_title')) {
 
 /**
  * Customized Titles
- * @param $title
+ * @param  $title
  * @return mixed
  */
-function podium_archive_title($title)
-{
-    if (is_category()) {
-        /* translators: Category archive title. 1: Category name */
-        $title = single_cat_title('', false);
-    } elseif (is_tag()) {
-        /* translators: Tag archive title. 1: Tag name */
-        $title = single_tag_title('', false);
-    } elseif (is_author()) {
-        /* translators: Author archive title. 1: Author name */
-        // $title = '<span class="vcard">' . get_the_author() . '</span>';
-    } elseif (is_year()) {
-        /* translators: Yearly archive title. 1: Year */
-        // $title = sprintf(__('Year: %s'), get_the_date(_x('Y', 'yearly archives date format')));
-    } elseif (is_month()) {
-        /* translators: Monthly archive title. 1: Month name and year */
-        // $title = sprintf(__('Month: %s'), get_the_date(_x('F Y', 'monthly archives date format')));
-    } elseif (is_day()) {
-        /* translators: Daily archive title. 1: Date */
-        // $title = sprintf(__('Day: %s'), get_the_date(_x('F j, Y', 'daily archives date format')));
-    } elseif (is_tax('post_format')) {
+    function podium_archive_title($title)
+    {
+        if (is_category()) {
 
-        // if (is_tax('post_format', 'post-format-aside')) {
-        //     $title = _x('Asides', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-gallery')) {
-        //     $title = _x('Galleries', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-image')) {
-        //     $title = _x('Images', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-video')) {
-        //     $title = _x('Videos', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-quote')) {
-        //     $title = _x('Quotes', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-link')) {
-        //     $title = _x('Links', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-status')) {
-        //     $title = _x('Statuses', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-audio')) {
-        //     $title = _x('Audio', 'post format archive title');
-        // } elseif (is_tax('post_format', 'post-format-chat')) {
-        //     $title = _x('Chats', 'post format archive title');
-        // }
+            // translators: Category archive title. 1: Category name
+            $title = single_cat_title('', false);
 
-    } elseif (is_post_type_archive()) {
-        /* translators: Post type archive title. 1: Post type name */
-        $title = post_type_archive_title('', false);
-    } elseif (is_tax()) {
-        $tax = get_taxonomy(get_queried_object()->taxonomy);
-        /* translators: Taxonomy term archive title. 1: Taxonomy singular name, 2: Current taxonomy term */
-        $title = single_term_title('', false);
-    } else {
-        $title = __('Archives');
+        } elseif (is_tag()) {
+
+            // translators: Tag archive title. 1: Tag name
+            $title = single_tag_title('', false);
+
+        } elseif (is_author()) {
+
+// translators: Author archive title. 1: Author name
+            // $title = '<span class="vcard">' . get_the_author() . '</span>';
+
+        } elseif (is_year()) {
+
+// translators: Yearly archive title. 1: Year
+            // $title = sprintf(__('Year: %s'), get_the_date(_x('Y', 'yearly archives date format')));
+
+        } elseif (is_month()) {
+
+// translators: Monthly archive title. 1: Month name and year
+            // $title = sprintf(__('Month: %s'), get_the_date(_x('F Y', 'monthly archives date format')));
+
+        } elseif (is_day()) {
+
+// translators: Daily archive title. 1: Date
+            // $title = sprintf(__('Day: %s'), get_the_date(_x('F j, Y', 'daily archives date format')));
+
+        } elseif (is_tax('post_format')) {
+
+// if (is_tax('post_format', 'post-format-aside')) {
+
+//     $title = _x('Asides', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-gallery')) {
+
+//     $title = _x('Galleries', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-image')) {
+
+//     $title = _x('Images', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-video')) {
+
+//     $title = _x('Videos', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-quote')) {
+
+//     $title = _x('Quotes', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-link')) {
+
+//     $title = _x('Links', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-status')) {
+
+//     $title = _x('Statuses', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-audio')) {
+
+//     $title = _x('Audio', 'post format archive title');
+
+// } elseif (is_tax('post_format', 'post-format-chat')) {
+
+//     $title = _x('Chats', 'post format archive title');
+            // }
+
+        } elseif (is_post_type_archive()) {
+
+            // translators: Post type archive title. 1: Post type name
+            $title = post_type_archive_title('', false);
+
+        } elseif (is_tax()) {
+
+            $tax = get_taxonomy(get_queried_object()->taxonomy);
+
+            // translators: Taxonomy term archive title. 1: Taxonomy singular name, 2: Current taxonomy term
+            $title = single_term_title('', false);
+
+        } else {
+
+            $title = __('Archives');
+
+        }
+
+        return $title;
     }
 
-    return $title;
 }
 
 add_filter('get_the_archive_title', 'podium_archive_title');
