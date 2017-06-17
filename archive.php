834bac76fd677b080156f41d53c88d6959757f9b
Amit Tal <amitprobable@gmail.com>
2017-06-17 20:23:33 +0300

    Better support for Custom Post Types - close issue #17

--- archive.php
+++ archive.php
@@ -42,7 +42,8 @@ if (have_posts()) {
          * If you want to override this in a child theme, then include a file
          * called content-___.php (where ___ is the Post Format name) and that will be used instead.
          */
-        get_template_part('template-parts/content', get_post_format());
+        get_template_part('template-parts/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
+
     }
 
 // End while
--- single.php
+++ single.php
@@ -23,7 +23,7 @@ $contentWidth = $settings->getContentClass('medium-8', 'medium-12');
 
         the_post();
 
-        get_template_part('template-parts/content', 'single');
+        get_template_part('template-parts/content-single', get_post_type());
 
         the_post_navigation();
 
