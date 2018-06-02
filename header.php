6676eaf7718ff783a332f05ecf5f13695c82e351
Amit Tal <amitprobable@gmail.com>
2018-06-02 20:02:30 +0300

    favicon code moved and now is a template part

--- header.php
+++ header.php
@@ -18,32 +18,11 @@ $settings = new settings();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="//gmpg.org/xfn/11">
 
-    <?php wp_head();
-
-    $imagesFolder = get_template_directory_uri() . '/dist/images/';
-
-// Please create favicon files with http://iconogen.com/
-// and put them in assets/images/favicon directory
+    <?php
+    wp_head();
+    get_template_part('template-parts/layouts/favicon', '');
     ?>
 
-    <link rel="shortcut icon" href="<?php echo $imagesFolder; ?>favicon/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/android-chrome-192x192.png" sizes="192x192">
-    <meta name="msapplication-square70x70logo" content="<?php echo $imagesFolder; ?>favicon/smalltile.png" />
-    <meta name="msapplication-square150x150logo" content="<?php echo $imagesFolder; ?>favicon/mediumtile.png" />
-    <meta name="msapplication-wide310x150logo" content="<?php echo $imagesFolder; ?>favicon/widetile.png" />
-    <meta name="msapplication-square310x310logo" content="<?php echo $imagesFolder; ?>favicon/largetile.png" />
 </head>
 
 <body <?php body_class(); ?>>
@@ -56,14 +35,14 @@ $settings = new settings();
         <header id="masthead" class="site-header" role="banner" data-sticky-container>
 
             <div class="show-for-medium top-bar" data-sticky data-margin-top="0">
-                        <div class="top-bar-right top-bar-menu">
-                            <?php $settings->getMenu(new Top_Bar_Walker(), 'onCanvass'); // print menu (source config.php) ?>
-                        </div>
-                        <div class="top-bar-left text-left top-bar-logo">
-                            <a href="<?php echo get_home_url(); ?>">
-                                <span class="title-bar-title"><?php echo get_bloginfo('name'); ?></span>
-                            </a>
-                        </div>
+                <div class="top-bar-right top-bar-menu">
+                    <?php $settings->getMenu(new Top_Bar_Walker(), 'onCanvass'); // print menu (source config.php) ?>
+                </div>
+                <div class="top-bar-left text-left top-bar-logo">
+                    <a href="<?php echo get_home_url(); ?>">
+                        <span class="title-bar-title"><?php echo get_bloginfo('name'); ?></span>
+                    </a>
+                </div>
             </div>
 
             <div class="hide-for-medium">
---
+++ template-parts/layouts/favicon.php
@@ -0,0 +1,26 @@
+<?php
+
+$imagesFolder = get_template_directory_uri() . '/dist/images/';
+
+// Please create favicon files with http://iconogen.com/
+// and put them in assets/images/favicon directory
+?>
+
+<link rel="shortcut icon" href="<?php echo $imagesFolder; ?>favicon/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $imagesFolder; ?>favicon/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo $imagesFolder; ?>favicon/android-chrome-192x192.png" sizes="192x192">
+<meta name="msapplication-square70x70logo" content="<?php echo $imagesFolder; ?>favicon/smalltile.png" />
+<meta name="msapplication-square150x150logo" content="<?php echo $imagesFolder; ?>favicon/mediumtile.png" />
+<meta name="msapplication-wide310x150logo" content="<?php echo $imagesFolder; ?>favicon/widetile.png" />
+<meta name="msapplication-square310x310logo" content="<?php echo $imagesFolder; ?>favicon/largetile.png" />
